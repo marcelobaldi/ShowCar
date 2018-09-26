36a0065c16d9ade42a2adfac4d14707b71a71caa
DESKTOP-GTS5OD6\Marcelo <marcelobaldi@hotmail.com>
2018-09-26 13:07:11 -0300

    dasfadsf

--- app/src/main/java/br/com/baldi/showcar/fragments/LoginFragment.java
+++ app/src/main/java/br/com/baldi/showcar/fragments/LoginFragment.java
@@ -7,14 +7,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import br.com.baldi.showcar.R;
 
 public class LoginFragment extends Fragment {
 
 
+    protected Button btnLogin;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate( R.layout.login_fragment, container, false);
+        View rootView = inflater.inflate( R.layout.login_fragment, container, false);
+        initComponents( rootView );
+        return rootView;
     }
+
+    private void initComponents(View view){
+        btnLogin = view.findViewById( R.id.btn_login );
+        btnLogin.setOnClickListener( onClick );
+    }
+
+    View.OnClickListener onClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getContext(), "LOGIN OK", Toast.LENGTH_SHORT).show();
+
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .replace( R.id.fragment_container, new ListFragment() )
+                    .commit();  //excecutando a operação (chamando);
+
+
+        }
+    };
 }
