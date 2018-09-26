e3eb1a921981ccdc6c83a37603460a545da97632
DESKTOP-GTS5OD6\Marcelo <marcelobaldi@hotmail.com>
2018-09-26 12:34:44 -0300

    Exemplo com fragments estatico

---
+++ app/src/main/java/br/com/baldi/showcar/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.baldi.showcar;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView(R.layout.main_activity);
+
+        //Chamar o Fragment Manager
+       // getSupportFragmentManager().beginTransaction().add(  )
+
+
+
+
+
+
+    }
+}
